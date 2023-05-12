eafc42cdb2b9cd1aa57c2d4bc41e440ac4aa348b
papupupu <2910935654@qq.com>
2023-05-12 23:16:05 +0800

    test for .gitignore

---
+++ class/build/CMakeFiles/main.dir/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/main.cpp.o: \
+ /home/papupupu/Learn_code/c++/cpp_yushiqi/class/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/papupupu/Learn_code/c++/cpp_yushiqi/class/student.h
